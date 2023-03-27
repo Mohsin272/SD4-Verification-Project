03f49d2b5e8e376f4371b322fb4cdc76d54f0cad
Mohsin Tahir <mohsintahir999@gmail.com>
2023-03-27 11:53:40 +0100

    added code for VISITOR test cases

--- src/cme/Rate.java
+++ src/cme/Rate.java
@@ -99,10 +99,21 @@ public class Rate {
     public BigDecimal calculate(Period periodStay) {
         int normalRateHours = periodStay.occurences(normal);
         int reducedRateHours = periodStay.occurences(reduced);
-        return (this.hourlyNormalRate.multiply(BigDecimal.valueOf(normalRateHours))).add(
-                this.hourlyReducedRate.multiply(BigDecimal.valueOf(reducedRateHours)));
-//        BigDecimal total = this.hourlyNormalRate.multiply(BigDecimal.valueOf(normalRateHours)).add(
+//        return (this.hourlyNormalRate.multiply(BigDecimal.valueOf(normalRateHours))).add(
 //                this.hourlyReducedRate.multiply(BigDecimal.valueOf(reducedRateHours)));
+        BigDecimal total = this.hourlyNormalRate.multiply(BigDecimal.valueOf(normalRateHours)).add(
+                this.hourlyReducedRate.multiply(BigDecimal.valueOf(reducedRateHours)));
+        if (kind == CarParkKind.VISITOR) {
+            BigDecimal freeAmount = BigDecimal.valueOf(10);
+            BigDecimal fiftyPercent = BigDecimal.valueOf(0.5);
+            if (total.compareTo(freeAmount) <= 0) { // if total is less than freeAmount then return total
+                return total;
+            } else { // If total is greater than 10, then apply 50% reduction above the 10
+                BigDecimal remainingCost = total.subtract(freeAmount).multiply(fiftyPercent);
+                return remainingCost;
+            }
+        }
+        return BigDecimal.ZERO;
 
     }
 }
