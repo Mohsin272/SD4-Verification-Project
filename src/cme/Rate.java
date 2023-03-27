1735d06267acd9709ef672e1bccd4dee905f7a15
Mohsin Tahir <mohsintahir999@gmail.com>
2023-03-27 12:31:30 +0100

    added code for STUDENT

--- src/cme/Rate.java
+++ src/cme/Rate.java
@@ -122,6 +122,19 @@ public class Rate {
                 return total;
             }
         }
+        else if (kind==CarParkKind.STUDENT) {
+            BigDecimal reductionAmount = BigDecimal.valueOf(5.50);
+            BigDecimal reductionPercent = BigDecimal.valueOf(0.33);
+            if (total.compareTo(reductionAmount) <= 0) {// if total is less than 5.50 then return total
+                return total;
+            }
+            else { // If total is greater than 5.50, then apply 33% reduction above the 5.50
+                BigDecimal remainder=total.subtract(reductionAmount);//get how much over it is 5.50
+                BigDecimal remainderCost=remainder.multiply(reductionPercent);//apply 33% to that amount over 5.50
+                BigDecimal totalReductionAmount=remainder.subtract(remainderCost);//take that amount from the amount over 5.50
+                return totalReductionAmount.add(reductionAmount);//add 5.50 and the reduced amount
+            }
+        }
         return BigDecimal.ZERO;
 
     }
